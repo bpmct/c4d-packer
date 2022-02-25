05553ecc00751eb14e6dd220f4dd366ce1531997
Ben Potter <me@bpmct.net>
2022-02-25 15:30:46 -0600

    Update 015-coder.sh

--- files/scripts/015-coder.sh
+++ files/scripts/015-coder.sh
@@ -2,4 +2,4 @@
 
 cd $HOME
 git clone https://github.com/bpmct/c4d-packer $HOME/coder/
-cd $HOME/coder && INITIAL_PASSWORD=coder docker-compose up -d
+cd $HOME/coder && INITIAL_PASSWORD=coder12345 docker-compose up -d
