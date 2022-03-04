fc853b1ad794605dd1f3e3f95c9ed5497b2ac90b
Ben Potter <me@bpmct.net>
2022-03-03 20:30:48 -0600

    fix firewall & support CVM workspaces

--- files/scripts/014-ufw-docker.sh
+++ files/scripts/014-ufw-docker.sh
@@ -10,4 +10,9 @@ ufw allow 2376/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
 
+# allow workspace containers to access coder over HTTP
+# however, 7080 is limited to the docker network interface
+# in docker-compose.yaml
+ufw allow 7080/tcp
+
 ufw --force enable
--- files/scripts/015-coder.sh
+++ files/scripts/015-coder.sh
@@ -2,4 +2,36 @@
 
 cd $HOME
 git clone https://github.com/bpmct/c4d-packer $HOME/coder/
-cd $HOME/coder && INITIAL_PASSWORD=coder12345 docker-compose up -d
+cd $HOME/coder && INITIAL_PASSWORD=temp_coder12345 docker-compose up -d
+
+# wait for Coder to be ready
+until [ "`docker inspect -f {{.State.Running}} $(docker-compose ps -q coder)`"=="true" ]; do
+    sleep 0.2;
+done;
+
+# Log in using the default user and password
+output=$(curl 'https://127.0.0.1/auth/basic/login' \
+  --data-raw '{"email":"admin","password":"temp_coder12345"}' \
+  --compressed \
+  --insecure)
+
+session_token="$(
+    echo "$output" | \
+    grep "session_token" | \
+    sed -e 's/"//g' -e 's/ //g' -e 's/,//g' | \
+    cut -d ":" -f2
+)"
+
+# change Docker provider access URL to support CVM workspaces
+curl --insecure  \
+    -i -H "Session-Token: $session_token" \
+    -X PUT 'https://127.0.0.1/api/private/workspace-providers/docker' \
+    --data-raw '{"name":"Docker","org_whitelist":["default"],"access_url":"http://172.17.0.1:7080","docker":{"api_uri":"unix:///var/run/docker.sock"}}'
+
+# set temporary password instead of the default one
+# this will also show the license prompt during intiial log in
+curl --insecure \
+    -i -H "Content-Type: application/json" \
+    -H "Session-Token: $session_token" \
+    -X PATCH "https://127.0.0.1/api/v0/users/admin" \
+    --data '{"old_password":"temp_coder12345","password":"coder12345", "temporary_password": true}'
